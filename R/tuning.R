c687249051ad3a0461c5c3a081727f373aac3eaa
ztstat <zijtian0418@gmail.com>
2026-02-08 22:51:14 -0800

    chore: remove unused TODO stubs

--- R/tuning.R
+++
@@ -1,9 +0,0 @@
-# Tuning parameter selection (pseudo-BIC, grid search, etc.)
-
-pseudo_bic <- function(theta_hat, S, n) {
-  stop("TODO")
-}
-
-select_lambda_by_bic <- function(fit_fn, S, n, lambda_grid) {
-  stop("TODO")
-}
